3d2f71caecf4b2b45ecb2e2140b47d9f9e0a506f
Kovalev Maksim <teasec4@yandex.ru>
2025-12-14 09:02:04 +0800

    test: GetProduct SQL

--- ecomm-api/storer/storer_mysql_test.go
+++ ecomm-api/storer/storer_mysql_test.go
@@ -42,5 +42,36 @@ func TestCreateProduct(t *testing.T){
 }
 
 func TestGetProduct(t *testing.T){
+	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil{
+		t.Fatalf("error creating mock database %v", err)
+	}
+	defer mockDB.Close()
+	// init DB
+	db := sqlx.NewDb(mockDB, "sqlmock")
+	// init storer 
+	st := NewMySQLStorer(db)
 	
+	p := &Product{
+		Name:      "test product",
+		Image:     "test_image.jpg",
+		Category: "test category",
+		Description: "test description",
+		Rating: 5,
+		NumReviews: 10,
+		Price:     10.99,
+		CountInStock:     100.0,
+	}
+	
+	rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"})
+	rows.AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
+	
+	mock.ExpectQuery("SELECT * FROM products WHERE id = ?").WithArgs(1).WillReturnRows(rows)
+	
+	gp, err := st.GetProduct(context.Background(), 1)
+	require.NoError(t, err)
+	require.Equal(t, int64(1), gp.ID)
+	
+	err = mock.ExpectationsWereMet()
+	require.NoError(t, err)
 }
