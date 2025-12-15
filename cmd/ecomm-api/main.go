f080d9b9769c991183cdc42dbacb7c6b4f890357
Kovalev Maksim <teasec4@yandex.ru>
2025-12-15 17:32:58 +0800

    implement token and session

--- cmd/ecomm-api/main.go
+++ cmd/ecomm-api/main.go
@@ -7,9 +7,17 @@ import (
 	"github.com/teasec4/ecomm-go-backend/ecomm-api/handler"
 	"github.com/teasec4/ecomm-go-backend/ecomm-api/server"
 	"github.com/teasec4/ecomm-go-backend/ecomm-api/storer"
+	"github.com/ianschenck/envflag"
+
 )
 
+const minSecrtKeySize = 32
+
 func main(){
+	var secretKey = envflag.String("SECRET_KEY", "01234567890123456789012345678914567891", "secret key for JWT")
+	if len(*secretKey) < minSecrtKeySize{
+		log.Fatalf("secret key must be at least %d characters long", minSecrtKeySize)
+	}
 	db, err := db.NewDatabase()
 	if err != nil{
 		log.Fatalf("error opening db: %v", err)
@@ -19,7 +27,7 @@ func main(){
 	
 	st := storer.NewMySQLStorer(db.GetDB())
 	serv := server.NewServer(st)
-	ndl := handler.NewHandler(serv)
+	ndl := handler.NewHandler(serv, *secretKey)
 	handler.RegisterRoutes(ndl)
 	handler.Start(":8080")
 }
--- db/migrations/20251212025557_add_users_table.up.sql
+++ db/migrations/20251212025557_add_users_table.up.sql
@@ -1,12 +1,10 @@
-CREATE TABLE `users`(
-    `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `is_admin` bool NOT NULL DEFAULT false
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` bool NOT NULL DEFAULT false,
+  `created_at` datetime DEFAULT (now()),
+  `updated_at` datetime,
+  UNIQUE (email)
 );
-
-ALTER TABLE `orders`
-    ADD COLUMN `user_id` int NOT NULL,
-    ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`)
-        REFERENCES `users` (`id`);
